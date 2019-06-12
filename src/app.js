1fa940ce15b7636b9755aee0f0b2ae486c6215f8
Tom Hinton <tomjhinton@gmail.com>
2019-06-12 11:46:20 +0100

    react

---
+++ src/app.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'bulma'
+import {HashRouter as Router, Route, Switch} from 'react-router-dom'
+
+import './style.scss'
+
+
+import Home from './components/Home'
+
+
+class App extends React.Component {
+  render(){
+    return (
+      <Router>
+        <main>
+
+          <Switch>
+            <Route path="/" component={Home} />
+
+          </Switch>
+        </main>
+      </Router>
+
+
+    )
+  }
+
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
---
+++ src/components/Home.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+
+class Home extends React.Component{
+  constructor(){
+    super()
+
+    this.state = {
+
+    }
+  }
+
+
+
+
+
+
+  render() {
+    return(
+      <main className="home-main">
+        <div>Test</div>
+
+
+      </main>
+
+
+    )
+  }
+}
+
+export default Home
