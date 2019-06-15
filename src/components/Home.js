1cb245c64439da9672cf53c28bce6f062e1c6ece
Tom Hinton <tomjhinton@gmail.com>
2019-06-15 21:52:46 +0100

    garmin.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import  Granim from 'granim'
+
+
 
 
 
@@ -12,15 +15,42 @@ class Home extends React.Component{
     }
   }
 
+  componentDidMount() {
+    const granimInstance = new Granim({
+      element: '#canvas-complex',
+      direction: 'diagonal',
+      isPausedWhenNotInView: true,
+      states: {
+        'default-state': {
+          gradients: [
+            [
+              { color: '#833ab4', pos: .2 },
+              { color: '#fd1d1d', pos: .8 },
+              { color: '#38ef7d', pos: 1 }
+            ], [
+              { color: '#40e0d0', pos: 0 },
+              { color: '#ff8c00', pos: .2 },
+              { color: '#ff0080', pos: .75 }
+            ]
 
+          ]
+        }
+      }
+    })
 
+  }
 
 
 
   render() {
+
+
+
     return(
       <main className="home-main">
-        <div>Home</div>
+        <div className='container'>
+          <h1 className="title is-1">Tom Hinton</h1>        </div>
+        <canvas id="canvas-complex"></canvas>
 
 
       </main>
